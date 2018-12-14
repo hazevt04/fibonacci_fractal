8d57c71af487599198f5367695cdd4faabe1f3dd
Glenn Hazelwood <hazelnutvt04@gmail.com>
2018-12-13 22:47:11 -0500

    Added InputParser class and cpp for fibonacci_fractal. No class for fibonacci_fractal yet

---
+++ utils.h
@@ -0,0 +1,27 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+/*
+#define VERBOSE_PRINTF( fmt, ... )        \
+   {                                      \
+         if ( verbose_flag ) {            \
+            printf( fmt, ##__VA_ARGS__ ); \
+      }                                   \
+   }
+
+
+#define CHECK_NULL_PTR( ptr )                                 \
+   {                                                          \
+         if ( !ptr ) {                                        \
+            std::cerr <<                                  \
+                     "File << #ptr << " Line "%d in %s(): ERROR: " #ptr \
+                     " is NULL! malloc failed?\n",            \
+                     __FILE__, __LINE__, __func__ );          \
+            exit( EXIT_FAILURE );                             \
+      }                                                       \
+   }
+
+
+*/
+
+#endif
