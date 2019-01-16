d4f7629eaf20a306d38e7c57176758bea2a15630
Glenn Hazelwood <hazelnutvt04@gmail.com>
2019-01-15 20:41:59 -0500

    Removed display of inputs from main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,18 @@ typedef unsigned long ulong;
 
 
 int main( int argc, char** argv ) {
-
    FibFractalInputParser* parser = new FibFractalInputParser();
    parser->parseInput( argc, argv );
    
    FibFractalInput* input = parser->getInput();
-   input->display();
 
    FibFractalBuilder* builder = new FibFractalBuilder( input );
+
    builder->BuildFractal( );
    
    Fractal* fractal = builder->GetFractal( );
    ImageData* image_data = fractal->Getimage_data( );
+   
    PNGRenderer* renderer = new PNGRenderer( image_data );
    
    renderer->doRender( );
